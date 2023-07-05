acb5cec6037a7890334090693edc8b6d28842fd5
danafan <13067882143@163.com>
2023-07-05 18:13:09 +0800

    增加额外任务验证

--- src/pages/order_detail.vue
+++ src/pages/order_detail.vue
@@ -7,6 +7,13 @@
 			</div>
 			<div class="content_box requirements">{{task_info.remark}}</div>
 		</div>
+		<div class="content" v-if="this.task_info.extra_goods_num > 0">
+			<div class="content_title">
+				<div class="tag_img"></div>
+				<div class="title_text">额外任务要求</div>
+			</div>
+			<div class="content_box requirements">{{task_info.extra_goods_remark}}</div>
+		</div>
 		<div class="cancel" @click="cancelOrder">取消任务</div>
 		<div class="content">
 			<div class="content_title">
@@ -62,133 +69,133 @@
 	</div>
 </template>
 <style lang="less" scoped>
-.container{
-	padding: .28rem .2rem;
-	.content{
-		margin-bottom: .28rem;
-		border-radius: .16rem;
-		background: #fff;
-		width: 100%;
-		.content_title{
-			border-bottom: 1px solid #F8F8F8;
+	.container{
+		padding: .28rem .2rem;
+		.content{
+			margin-bottom: .28rem;
+			border-radius: .16rem;
+			background: #fff;
 			width: 100%;
-			display: flex;
-			align-items: center;
-			height: .78rem;
-			.tag_img{
-				margin-right: .2rem;
-				border-radius:.06rem;
-				background: #00C693;
-				width: .1rem;
-				height: .34rem;
+			.content_title{
+				border-bottom: 1px solid #F8F8F8;
+				width: 100%;
+				display: flex;
+				align-items: center;
+				height: .78rem;
+				.tag_img{
+					margin-right: .2rem;
+					border-radius:.06rem;
+					background: #00C693;
+					width: .1rem;
+					height: .34rem;
+				}
+				.title_text{
+					font-size: .3rem;
+					color: #333333;
+					font-weight:500;
+				}
 			}
-			.title_text{
-				font-size: .3rem;
-				color: #333333;
-				font-weight:500;
+			.content_box{
+				padding: .14rem .28rem;
 			}
-		}
-		.content_box{
-			padding: .14rem .28rem;
-		}
-		.requirements{
-			font-size: .28rem;
-			color:#333333;
-		}
-		.keyword{
-			display: flex;
-			flex-direction: column;
-			align-items: center;
-			.keyword_text{
-				margin-bottom: .2rem;
-				font-size: .32rem;
-				color:#00C693;
-				font-weight:500;
-				word-wrap:break-word;
-				-webkit-touch-callout:none;  
-				-webkit-user-select:none;  
-				-khtml-user-select:none;  
-				-moz-user-select:none;  
-				-ms-user-select:none;  
-				user-select:none;  
-			}
-			.goods_img{
-				border-radius:.08rem;
-				width: 4.6rem;
-				height: 4.6rem;
-			}
-		}
-		.goods_price{
-			font-size: .3rem;
-			color: #333333;
-		}
-		.submit{
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-			.input_box{
-				border:1px solid #F1F1F1;
-				border-radius: .08rem;
-				background: #f8f8f8;
-				width: 5.1rem;
-				height: .8rem;
-				padding-left: .26rem;
-				border: none;
-				outline: none;
+			.requirements{
 				font-size: .28rem;
-				box-sizing: border-box;
+				color:#333333;
 			}
-			.text_box{
-				border:1px solid #F1F1F1;
-				border-radius: .08rem;
-				background: #f8f8f8;
-				width: 5.1rem;
-				height: 1.6rem;
-				padding-left: .26rem;
-				border: none;
-				outline: none;
-				font-size: .28rem;
-				box-sizing: border-box;
-				padding: .1rem;
+			.keyword{
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				.keyword_text{
+					margin-bottom: .2rem;
+					font-size: .32rem;
+					color:#00C693;
+					font-weight:500;
+					word-wrap:break-word;
+					-webkit-touch-callout:none;  
+					-webkit-user-select:none;  
+					-khtml-user-select:none;  
+					-moz-user-select:none;  
+					-ms-user-select:none;  
+					user-select:none;  
+				}
+				.goods_img{
+					border-radius:.08rem;
+					width: 4.6rem;
+					height: 4.6rem;
+				}
 			}
 			.goods_price{
-				flex:1;
+				font-size: .3rem;
+				color: #333333;
 			}
-			input::-webkit-input-placeholder {
-				color: #666666;
-			}
-			input:-moz-placeholder {
-				color: #666666;
-			}
-			input:-ms-input-placeholder {
-				color: #666666;
-			}
-			.submit_but{
-				margin-left: .3rem;
-				border-radius:.06rem;
-				background: #00C693;
-				width: 1.2rem;
-				text-align: center;
-				height: .6rem;
-				line-height: .6rem;
-				font-size: .28rem;
-				color:#fff;
+			.submit{
+				display: flex;
+				align-items: center;
+				justify-content: space-between;
+				.input_box{
+					border:1px solid #F1F1F1;
+					border-radius: .08rem;
+					background: #f8f8f8;
+					width: 5.1rem;
+					height: .8rem;
+					padding-left: .26rem;
+					border: none;
+					outline: none;
+					font-size: .28rem;
+					box-sizing: border-box;
+				}
+				.text_box{
+					border:1px solid #F1F1F1;
+					border-radius: .08rem;
+					background: #f8f8f8;
+					width: 5.1rem;
+					height: 1.6rem;
+					padding-left: .26rem;
+					border: none;
+					outline: none;
+					font-size: .28rem;
+					box-sizing: border-box;
+					padding: .1rem;
+				}
+				.goods_price{
+					flex:1;
+				}
+				input::-webkit-input-placeholder {
+					color: #666666;
+				}
+				input:-moz-placeholder {
+					color: #666666;
+				}
+				input:-ms-input-placeholder {
+					color: #666666;
+				}
+				.submit_but{
+					margin-left: .3rem;
+					border-radius:.06rem;
+					background: #00C693;
+					width: 1.2rem;
+					text-align: center;
+					height: .6rem;
+					line-height: .6rem;
+					font-size: .28rem;
+					color:#fff;
+				}
 			}
 		}
+		.cancel{
+			margin-bottom: .28rem;
+			border:1px solid #00C693;
+			border-radius:.1rem;
+			background: #fff;
+			width: 100%;
+			text-align: center;
+			height: .9rem;
+			line-height: .9rem;
+			font-size: .3rem;
+			color:#00C693;
+		}
 	}
-	.cancel{
-		margin-bottom: .28rem;
-		border:1px solid #00C693;
-		border-radius:.1rem;
-		background: #fff;
-		width: 100%;
-		text-align: center;
-		height: .9rem;
-		line-height: .9rem;
-		font-size: .3rem;
-		color:#00C693;
-	}
-}
 </style>
 <script>
 	import { MessageBox } from 'mint-ui';
@@ -216,6 +223,14 @@
 				resource.taskDetail({usertask_id:this.task_id}).then(res => {
 					if(res.data.code == 1){
 						this.task_info = res.data.data;
+						if(this.task_info.extra_goods_num > 0){
+							MessageBox({
+								$type:'alert',
+								message:'本次任务由多任务组成，需要全部完成，详情可看任务要求!',
+								title:'温馨提示',
+								confirmButtonText:"我知道了"
+							})
+						}
 					}else{
 						this.$toast(res.data.msg);
 					}
@@ -303,14 +318,29 @@
 			},
 			//淘宝/拼多多/抖音提交
 			submitTask(){
-				resource.commitTask({usertask_id:this.task_id,}).then(res => {
-					if(res.data.code == 1){
-						this.$toast(res.data.msg);
-						this.$router.go(-2);
-					}else{
-						this.$toast(res.data.msg);
-					}
-				})
+				if(this.task_info.extra_goods_num > 0){
+					MessageBox.confirm(this.goods_price).then(action => {
+						if(action == 'confirm'){
+							resource.commitTask({usertask_id:this.task_id,}).then(res => {
+								if(res.data.code == 1){
+									this.$toast(res.data.msg);
+									this.$router.go(-2);
+								}else{
+									this.$toast(res.data.msg);
+								}
+							})
+						}
+					});
+				}else{
+					resource.commitTask({usertask_id:this.task_id,}).then(res => {
+						if(res.data.code == 1){
+							this.$toast(res.data.msg);
+							this.$router.go(-2);
+						}else{
+							this.$toast(res.data.msg);
+						}
+					})
+				}
 			}
 		}
 	}
